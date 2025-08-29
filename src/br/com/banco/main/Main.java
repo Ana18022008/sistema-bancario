c067c6bce5cbf1b81267fc35d1ab43a2edd1462a
Catarina Klein <catarina_klein@estudante.sesisenai.org.br>
2025-08-29 17:01:51 -0300

    feat(main): cria estrutura inicial da classe main

--- src/br/com/banco/main/Main.java
+++ src/br/com/banco/main/Main.java
@@ -1,4 +1,22 @@
 package br.com.banco.main;
 
+// importam os pacotes que serão utilizados
+import br.com.banco.model.Conta;
+import br.com.banco.service.Banco;
+import br.com.banco.view.Atendente;
+
 public class Main {
+    public static void  main(String[] args){ //inicia a main como static
+
+        Banco banco = new Banco(); // cria o objeto banco
+        Atendente atendente = new Atendente(banco); // cria o objeto atendente
+
+        int escolhaAcao = 1; // inicializa a variável escolhaAcao (1 é um número aleatório)
+
+        do{ // inicia o laço do while
+            escolhaAcao = atendente.escolhaAcao(); // solicita ao usuário que escolha uma ação no menu
+        } while (escolhaAcao != 0); // repete o menu enquanto o usuário não escolher sair
+
+        atendente.sair();  // exibe mensagem de saída do sistema
+    }
 }
